06ae230d2485d49eaffc53941aad2e6fd7ff0ab9
SebasUrbina <sebastian.urbina@live.cl>
2025-05-02 22:39:04 -0400

    ✨ feat: remove React import from BlogList component

--- src/components/blog/BlogList.tsx
+++ src/components/blog/BlogList.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { BlogMetadata } from "../../types/Blog";
 import BlogCard from "./BlogCard";
 
